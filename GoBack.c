68c5e185afd741c4a31a1712d8c509b60ebec95a
Prathama Pratihar <pratihar.prathama@gmail.com>
2025-04-20 23:35:29 +0530

    Added the code for Go Back N ARQ in C

---
+++ GoBack.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define MAX_FRAMES 100
+
+int window_size, total_frames;
+
+int main(){
+	srand(time(0));
+	printf("Enter the number of total frames:\n");
+	scanf("%d", &total_frames);
+	printf("Enter the window size: \n");
+	scanf("%d", &window_size);
+	
+
+
+
+}
+
+
+
